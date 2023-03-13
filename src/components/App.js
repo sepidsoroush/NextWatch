199b3a43309dbbc2c3eddcbbe33ba1028a387eda
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-14 01:32:28 +0200

    set movie info as states

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-import axios from "axios";
-const handleSubmit = ()=>{
-    axios.get('http://www.omdbapi.com/?apikey=6749959a&s=inception&type=movie&y=2010')
-    .then(response => console.log(response.data.Search))
-    .catch(error => console.log(error));
-}
+import useState from 'react';
+import axios from "axios"
 
 const App = ()=>{
+//declare variables
+const [info , setInfo] = useState({
+    id : '',
+    title : '',
+    year : '',
+    type : '',
+    poster : ''
+});
+
+const handleSubmit = ()=>{
+    axios.get(`http://www.omdbapi.com/?apikey=6749959a&s=${info.title}&type=${info.type}&y=${info.year}`)
+    .then((response) => {
+        const searchedMovies = response.data.Search;
+        setInfo({
+            id : searchedMovies.imdbID ,
+            title : searchedMovies.Title ,
+            year : searchedMovies.Year ,
+            type : searchedMovies.Type ,
+            poster : searchedMovies.Poster
+        })
+        console.log(response.data.Search)})
+    .catch((error) => {
+        console.log(error)});
+}
     return(
         <div>
+            
             <button
             onClick={handleSubmit}
             >search</button>
