c0e6d1f83a9a1d170bf27eacd73bfd250de065b8
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-15 13:54:04 +0200

    call API to get selected movie complete data

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -1,8 +1,23 @@
 import {useRouter} from 'next/router'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 export default function Movie() {
     const router = useRouter()
     const {id} = router.query
+
+    const findMovie = ()=>{
+        axios
+            .get(`http://www.omdbapi.com/?apikey=6749959a&i=${id}`)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error)});
+    }; 
+    useEffect(()=>{
+        findMovie();
+    } , []);
+
     return (
         <h1>
             Test : {id}
