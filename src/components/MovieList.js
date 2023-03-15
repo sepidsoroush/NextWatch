12e88e5489fc93767d1b377fed9bcbc5639321e9
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-15 11:08:23 +0200

    map serachedMovies data in MovieList component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,31 +3,23 @@ import {useContext} from 'react';
 
 const MoviesList = () =>{
     const {searchedMovies} = useContext(AppContext);
-    console.log(searchedMovies);
     return(
         <div>
             <h1>movie list</h1>
-
+            {searchedMovies.map((item)=>{
+                const {imdbID,Title ,Year,Type,Poster} = item;
+                return(
+                    <ul>
+                        <li key={imdbID}>
+                            <p>{Title}</p>
+                            <p>{Year}</p>
+                            <p>{Type}</p>
+                            <img src={Poster} alt={Title} />
+                        </li>
+                    </ul>
+                )        
+            })}
         </div>
-    )
-    // 
-    // return(
-    //     <div>
-    //         {searchedMovies.map((movie)=>{
-    //             const {imdbID,Title ,Year,Type,Poster} = movie;
-    //             <ul>
-    //                 <li>
-    //                     <div>
-    //                         <p>{imdbID}</p>
-    //                         <h1>{Title}</h1>
-    //                         <h6>{Year}</h6>
-    //                         <h6>{Type}</h6>
-    //                         <img src={Poster} alt={Title} />
-    //                     </div>
-    //                 </li>
-    //             </ul>
-    //         })}
-    //     </div>
-    // )
+    );
 }
 export default MoviesList;
