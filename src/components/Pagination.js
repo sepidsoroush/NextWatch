26ee089881205c149b580b86a3ff5d1df27818f3
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-17 09:51:37 +0200

    update for loop of pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,15 +5,15 @@ const Pagination = () =>{
     const {results , input , setInput} = useContext(AppContext)
     const totalPages = Math.ceil(results/10)
     let numberList = [];
-    for (let i=0 ; i< totalPages ; i++){
+    for (let i=1 ; i< totalPages+1 ; i++){
         numberList.push(i)
     }
     return (
         <div>
             <ul>
-                {numberList.map((number , index)=>{
+                {numberList.map((number)=>{
                     return(
-                        <li key={index}>{number}</li>
+                        <li key={number}>{number}</li>
                     )
                 })}
             </ul>
