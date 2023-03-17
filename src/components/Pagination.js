9475752186f5f15dc541c516a64ade0396bf2f79
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-17 12:15:30 +0200

    add prev & next page buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,28 +1,51 @@
 import { AppContext } from '@/components/context'
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 
 const Pagination = () =>{
-    const {results , input , setInput , getData} = useContext(AppContext)
+    const {results , input , setInput} = useContext(AppContext)
     const totalPages = Math.ceil(results/10)
     let numberList = [];
+    let currentPage = parseInt(input.page);
     for (let i=1 ; i< totalPages+1 ; i++){
         numberList.push(i)
     }
+
+    const [pages , setPages] = useState(currentPage)
+
+    const prevPage = () =>{
+        setPages((currentPage)=>{
+            let prevPage = currentPage - 1;
+            if(prevPage <= 0){
+                prevPage = 1 ;
+            }
+            return(prevPage)
+        })
+    }
+
+    function nextPage() {
+        setPages((currentPage)=>{
+            let nextPage = currentPage + 1 
+            if( nextPage > totalPages){
+                nextPage = totalPages;
+            }
+            return (nextPage)
+        })
+    }
+    console.log(pages)
     return (
         <div>
+            <button onClick={prevPage}>Prev</button>
             {numberList.map((number , index)=>{
                 return(
                     <button 
                     key={index}
                     value={number}
-                    onClick={
-                        (event) => setInput({...input , page : event.target.value})
-                    }
-                    >
+                    onClick={(event) => setInput({...input , page : event.target.value})}>
                         {number}
                     </button>
                 )
             })}
+            <button onClick={nextPage}>Next</button>
         </div>
     )
     
