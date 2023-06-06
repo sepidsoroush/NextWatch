4f55dbf2ee227ef87ef837a955640e18229a636d
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-06-06 10:47:11 +0300

    update Footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,11 +1,44 @@
+import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
 import { Inter } from "next/font/google";
+
 const inter = Inter({ subsets: ["latin"] });
 
-const Footer = () => {
+function Footer() {
   return (
-    <div className={inter.className}>
-      <h1>Footer</h1>
-    </div>
+    <footer className={`${styles.footer} ${inter.className}`}>
+      <div className={styles.links}>
+        <Link href="/#" className={styles.item}>
+          About
+        </Link>
+        <Link href="/#" className={styles.item}>
+          FAQs
+        </Link>
+        <Link href="/#" className={styles.item}>
+          Contact Us
+        </Link>
+      </div>
+      <div className={styles.copyright}>
+        <span>
+          Coded by &nbsp;
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/sepidsoroush"
+          >
+            Sepideh Soroush
+          </a>
+        </span>
+        <span> | </span>
+        <span className={styles.copyright}>
+          The Movie Database by &nbsp;
+          <a target="_blank" rel="noreferrer" href="https://www.omdbapi.com/">
+            OMDB API
+          </a>
+        </span>
+      </div>
+    </footer>
   );
-};
+}
+
 export default Footer;
