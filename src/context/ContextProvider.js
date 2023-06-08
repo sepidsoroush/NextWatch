1cad0029e776f5c4d98636c848fe085790a47049
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-06-08 10:20:22 +0300

    added search context

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,12 +1,23 @@
 import { AppContext } from "./app-context";
 import { useState } from "react";
+import useFetch from "../hooks/use-fetch";
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const ContextProvider = (props) => {
-  const [watchlist, setWatchlist] = useState([]);
+  // search context
+  const [input, setInput] = useState({
+    title: "Batman",
+    page: 1,
+    type: "",
+  });
+  const { isLoading, error, searchedMovies } = useFetch(
+    `&s=${input.title}&type=${input.type}&page=${input.page}`
+  );
 
+  // watchlist context
+  const [watchlist, setWatchlist] = useState([]);
   const toggleWatchlist = (movie) => {
     const movieIndex = watchlist.findIndex((m) => m.imdbID === movie.imdbID);
-
     if (movieIndex !== -1) {
       setWatchlist((prevWatchlist) =>
         prevWatchlist.filter((m) => m.imdbID !== movie.imdbID)
@@ -19,6 +30,11 @@ const ContextProvider = (props) => {
   const value = {
     watchlist,
     toggleWatchlist,
+    isLoading,
+    error,
+    searchedMovies,
+    input,
+    setInput,
   };
 
   return (
